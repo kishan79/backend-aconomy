831ea98d96f2d35cc07fd8644d93781d1f3c5f4f
Kishan <kishan.237@gmail.com>
2023-05-22 21:41:29 +0530

    Added timezone and set time to run validation cron to 11AM ist

--- scheduler/validation.js
+++ scheduler/validation.js
@@ -8,49 +8,54 @@ const { isBefore } = require("date-fns");
 dotenv.config({ path: "./.env" });
 connectDB();
 
-cron.schedule("0 0 0 * * *", async function () {
-  try {
-    let nftData = await NftModel.find();
-    if (nftData.length) {
-      console.log(nftData.length);
-      for (let i = 0; i < nftData.length; i++) {
-        if (!nftData[i].validationExpired) {
-          const data = await NFTValidationModel.findOne({
-            assetOwnerAddress: nftData[i].nftOwnerAddress,
-            asset: nftData[i]._id,
-          });
-          if (data) {
-            // console.log(new Date(data.requestExpiresOn));
-            // console.log(isBefore(new Date(data.requestExpiresOn), new Date()));
-            if (data.requestExpiresOn) {
-              if (isBefore(new Date(data.requestExpiresOn), new Date())) {
-                let validationData = await NFTValidationModel.findOneAndUpdate(
-                  { _id: data._id },
-                  { validationExpired: true, requestState: "unvalidated" }
-                );
-                if (validationData) {
-                  let dataNft = await NftModel.findOneAndUpdate(
-                    { _id: nftData[i]._id },
-                    {
-                      validationExpired: true,
-                      validationState: "unvalidated",
-                    }
-                  );
-                  if (dataNft) {
-                    console.log(
-                      `Data Updated for tokenid ${nftData[i].tokenId}`
+cron.schedule(
+  "0 0 11 * * *",
+  async function () {
+    try {
+      let nftData = await NftModel.find();
+      if (nftData.length) {
+        console.log(nftData.length);
+        for (let i = 0; i < nftData.length; i++) {
+          if (!nftData[i].validationExpired) {
+            const data = await NFTValidationModel.findOne({
+              assetOwnerAddress: nftData[i].nftOwnerAddress,
+              asset: nftData[i]._id,
+            });
+            if (data) {
+              // console.log(new Date(data.requestExpiresOn));
+              // console.log(isBefore(new Date(data.requestExpiresOn), new Date()));
+              if (data.requestExpiresOn) {
+                if (isBefore(new Date(data.requestExpiresOn), new Date())) {
+                  let validationData =
+                    await NFTValidationModel.findOneAndUpdate(
+                      { _id: data._id },
+                      { validationExpired: true, requestState: "unvalidated" }
                     );
+                  if (validationData) {
+                    let dataNft = await NftModel.findOneAndUpdate(
+                      { _id: nftData[i]._id },
+                      {
+                        validationExpired: true,
+                        validationState: "unvalidated",
+                      }
+                    );
+                    if (dataNft) {
+                      console.log(
+                        `Data Updated for tokenid ${nftData[i].tokenId}`
+                      );
+                    }
                   }
                 }
               }
             }
           }
         }
+      } else {
+        console.log("Failed to fetch data");
       }
-    } else {
-      console.log("Failed to fetch data");
+    } catch (err) {
+      console.log(err);
     }
-  } catch (err) {
-    console.log(err);
-  }
-});
+  },
+  { timezone: "Asia/Kolkata" }
+);
