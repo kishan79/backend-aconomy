32cfcaa22250961b8782a66932e0904ae64370c7
Kishan <kishan.237@gmail.com>
2023-06-20 18:19:20 +0530

    Added check for empty string in global search

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -8,41 +8,48 @@ const asyncHandler = require("../middlewares/async");
 exports.globalSearch = asyncHandler(async (req, res, next) => {
   try {
     const { q } = req.query;
-    const nftData = await NftModel.find({
-      name: { $regex: q, $options: "i" },
-    }).select("_id name");
+    if (q !== "") {
+      const nftData = await NftModel.find({
+        name: { $regex: q, $options: "i" },
+      }).select("_id name");
 
-    const userData = await UserModel.find({
-      $or: [
-        { name: { $regex: q, $options: "i" } },
-        { username: { $regex: q, $options: "i" } },
-      ],
-    }).select("_id name username");
+      const userData = await UserModel.find({
+        $or: [
+          { name: { $regex: q, $options: "i" } },
+          { username: { $regex: q, $options: "i" } },
+        ],
+      }).select("_id name username");
 
-    const validatorData = await ValidatorModel.find({
-      $or: [
-        { name: { $regex: q, $options: "i" } },
-        { username: { $regex: q, $options: "i" } },
-      ],
-    }).select("_id name username");
+      const validatorData = await ValidatorModel.find({
+        $or: [
+          { name: { $regex: q, $options: "i" } },
+          { username: { $regex: q, $options: "i" } },
+        ],
+      }).select("_id name username");
 
-    const poolData = await PoolModel.find({
-      name: { $regex: q, $options: "i" },
-    }).select("_id name");
+      const poolData = await PoolModel.find({
+        name: { $regex: q, $options: "i" },
+      }).select("_id name");
 
-    const collectionData = await CollectionModel.find({
-      name: { $regex: q, $options: "i" },
-    }).select("_id name");
+      const collectionData = await CollectionModel.find({
+        name: { $regex: q, $options: "i" },
+      }).select("_id name");
 
-    res.status(200).json({
-      data: {
-        nft: nftData,
-        collection: collectionData,
-        user: userData,
-        validator: validatorData,
-        pool: poolData,
-      },
-    });
+      res.status(200).json({
+        data: {
+          nft: nftData,
+          collection: collectionData,
+          user: userData,
+          validator: validatorData,
+          pool: poolData,
+        },
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        data: {},
+      });
+    }
   } catch (err) {
     res.status(400).json({ success: false });
   }
